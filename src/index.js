e45f2f91230aacc6fe4d90790af9a045e1b5f28a
vlukyaneRubyLabs <vlukyane@gmail.com>
2018-02-24 10:17:54 +0300

    sixth task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
-}
+module.exports = function getZerosCount(n) {
+    var i = 5, count = 0;
+    while (n / i >= 1){
+        count += Math.floor(n / i);
+        i *= 5;
+    }
+    return count;
+};
